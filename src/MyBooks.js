28eceba7714b5ef74c49d2d59f7a42a28c78dfbe
Pforoni <paulo.foroni@gmail.com>
2017-12-04 00:55:20 -0200

    Corrigido o load do titulo da estante

--- src/MyBooks.js
+++ src/MyBooks.js
@@ -29,7 +29,9 @@ class MyBooks extends Component {
                 <div className="list-books-content">
                     <div>
                         <div className="bookshelf">
-                            <h2 className="bookshelf-title">Currently Reading</h2>
+                            {books.length > 0 && bookscurrentlyReading.length > 0 && (
+                                <h2 className="bookshelf-title">Currently Reading</h2>
+                            )}
                             <div className="bookshelf-books">
                                 <ol className="books-grid">
                                     {books.length > 0 && bookscurrentlyReading.length > 0 && bookscurrentlyReading.map((currentReading) =>
@@ -59,7 +61,9 @@ class MyBooks extends Component {
                             </div>
                         </div>
                         <div className="bookshelf">
-                            <h2 className="bookshelf-title">Want to Read</h2>
+                            {books.length > 0 && bookswantToRead.length > 0 && (
+                                <h2 className="bookshelf-title">Want to Read</h2>
+                            )}
                             <div className="bookshelf-books">
                                 <ol className="books-grid">
                                     {books.length > 0 && bookswantToRead.length > 0 && bookswantToRead.map((wantToRead) =>
@@ -89,32 +93,34 @@ class MyBooks extends Component {
                             </div>
                         </div>
                         <div className="bookshelf">
-                            <h2 className="bookshelf-title">Read</h2>
+                            {books.length > 0 && booksread.length > 0 && (
+                                <h2 className="bookshelf-title">Read</h2>
+                            )}
                             <div className="bookshelf-books">
                                 <ol className="books-grid">
-                                {books.length > 0 && booksread.length > 0 && booksread.map((read) =>
-                                    <li key={read.id}>
-                                        <div className="book">
-                                            <div className="book-top">
-                                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${read.imageLinks.thumbnail})` }}></div>
-                                                <div className="book-shelf-changer">
-                                                    <select>
-                                                        <option value="none" disabled>Move to...</option>
-                                                        <option value="currentlyReading">Currently Reading</option>
-                                                        <option value="wantToRead" >Want to Read</option>
-                                                        <option value="read" selected>Read</option>
-                                                        <option value="none">None</option>
-                                                    </select>
+                                    {books.length > 0 && booksread.length > 0 && booksread.map((read) =>
+                                        <li key={read.id}>
+                                            <div className="book">
+                                                <div className="book-top">
+                                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${read.imageLinks.thumbnail})` }}></div>
+                                                    <div className="book-shelf-changer">
+                                                        <select>
+                                                            <option value="none" disabled>Move to...</option>
+                                                            <option value="currentlyReading">Currently Reading</option>
+                                                            <option value="wantToRead" >Want to Read</option>
+                                                            <option value="read" selected>Read</option>
+                                                            <option value="none">None</option>
+                                                        </select>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            <div className="book-title">{read.title}</div>
-                                            {read.authors && read.authors.map((author) =>
-                                                <div key={author} className="book-authors">{author}</div>
-                                            )}
+                                                <div className="book-title">{read.title}</div>
+                                                {read.authors && read.authors.map((author) =>
+                                                    <div key={author} className="book-authors">{author}</div>
+                                                )}
 
-                                        </div>
-                                    </li>
-                                )}
+                                            </div>
+                                        </li>
+                                    )}
                                 </ol>
                             </div>
                         </div>
